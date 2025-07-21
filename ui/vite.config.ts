a6c5a1ab70946df718ad133f62b9e70c33921167
reon1do <faiteforever@gmail.com>
2025-07-21 18:41:50 +0300

    Updated vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.join(process.cwd(), "../"));
+  const API_URL = `${env.VITE_API_URL ?? "http://localhost:4200"}`;
+  const PORT = `${env.VITE_PORT ?? "3000"}`;
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(PORT),
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
